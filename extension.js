e62ae3855027b1498acd4bad024e6e1565feb295
Pacome Pilaz <papilaz@z4r12p4.42lyon.fr>
2025-12-12 13:07:09 +0100

    add all

--- extension.js
+++ extension.js
@@ -1,19 +1,19 @@
-import GObject from 'gi://GObject';
-import St from 'gi://St';
-import GLib from 'gi://GLib';
-import Clutter from 'gi://Clutter';
-import Soup from 'gi://Soup?version=3.0';
-import * as Main from 'resource:///org/gnome/shell/ui/main.js';
-import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
-import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
-import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
+const { St, GLib, Clutter, Soup, GObject } = imports.gi;
+const Main = imports.ui.main;
+const PanelMenu = imports.ui.panelMenu;
+const PopupMenu = imports.ui.popupMenu;
+const Me = imports.misc.extensionUtils.getCurrentExtension();
 
 // --- CONFIGURATION ---
 const UID = "u-s4t2ud-f03601c98c248d266a0ff1ad9fa690660dcc1e4a73f3db5d5210d98f44123e26";
 const SECRET = "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";
-const ME = "papilaz"; // <--- REMPLACE PAR TON LOGIN (ex: pacome)
+const ME = "papilaz"; // Ton login
 
-const _httpSession = new Soup.Session();
+// Sur GNOME 42 (Ubuntu 22.04), Soup est souvent en v2.4 par défaut via imports.gi.
+// On adapte la session pour être compatible.
+const _httpSession = new Soup.SessionAsync();
+// Fake user agent pour éviter certains blocages
+_httpSession.user_agent = 'LogtimeExtension/1.0';
 
 const DashboardIndicator = GObject.registerClass(
 class DashboardIndicator extends PanelMenu.Button {
@@ -29,6 +29,8 @@ class DashboardIndicator extends PanelMenu.Button {
         this.add_child(this.buttonLabel);
 
         // Menu
+        // Sur GNOME 42, on accède souvent à 'this.menu.box' différemment, 
+        // mais essayons de garder la structure standard.
         this.menu.box.style_class = 'lgt-popup-menu';
 
         // 1. Timer
@@ -44,7 +46,7 @@ class DashboardIndicator extends PanelMenu.Button {
 
         this.menu.box.add_child(new PopupMenu.PopupSeparatorMenuItem());
 
-        // Premier appel + boucle
+        // Refresh loop
         this._refresh();
         this._timeout = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 120, () => {
             this._refresh();
@@ -64,42 +66,39 @@ class DashboardIndicator extends PanelMenu.Button {
     async _getToken() {
         if (this.token && this.tokenExpire > (Date.now()/1000)) return this.token;
 
+        // Soup 2.4 syntax
         let msg = Soup.Message.new('POST', 'https://api.intra.42.fr/oauth/token');
         
-        // Utilisation du format JSON (comme ton extension web)
         let bodyObj = {
             grant_type: 'client_credentials',
             client_id: UID,
             client_secret: SECRET
         };
         let bodyStr = JSON.stringify(bodyObj);
-        let bodyBytes = new GLib.Bytes(new TextEncoder().encode(bodyStr));
         
-        msg.set_request_body_from_bytes('application/json', bodyBytes);
+        // Soup 2.4 set_request (différent de Soup 3)
+        msg.set_request('application/json', 2, bodyStr, bodyStr.length);
+
+        let response = await this._send_async(msg);
+        if (!response) return null;
 
         try {
-            let bytes = await this._send_async(msg);
-            // Décodage strict pour GNOME 46
-            let text = new TextDecoder().decode(bytes.get_data()); 
-            let data = JSON.parse(text);
-            
+            let data = JSON.parse(response);
             if (data.error) throw new Error(data.error);
 
             this.token = data.access_token;
             this.tokenExpire = (Date.now()/1000) + data.expires_in;
             return this.token;
         } catch (e) {
-            console.error("Token Error:", e);
-            let code = msg.status_code;
-            if (code && code !== 200) this.buttonLabel.set_text(`HTTP ${code}`);
-            else this.buttonLabel.set_text("ERR AUTH");
+            log("LogtimeAuthError: " + e);
+            this.buttonLabel.set_text("ERR AUTH");
             return null;
         }
     }
 
     async _refresh() {
         let token = await this._getToken();
-        if (!token) return; // L'erreur est déjà affichée par _getToken
+        if (!token) return;
 
         let now = new Date();
         let start = new Date(now.getFullYear(), now.getMonth(), 1).toISOString();
@@ -143,15 +142,17 @@ class DashboardIndicator extends PanelMenu.Button {
 
     _send_async(msg) {
         return new Promise((resolve, reject) => {
-            _httpSession.send_and_read_async(msg, GLib.PRIORITY_DEFAULT, null, (session, result) => {
-                try {
-                    let bytes = session.send_and_read_finish(result);
-                    if (msg.status_code !== 200 && msg.status_code !== 201) {
-                         reject(new Error("HTTP " + msg.status_code));
+            _httpSession.queue_message(msg, (session, message) => {
+                if (message.status_code !== 200 && message.status_code !== 201) {
+                    log("API Error: " + message.status_code);
+                    resolve(null);
+                } else {
+                    if (message.response_body && message.response_body.data) {
+                        resolve(message.response_body.data);
                     } else {
-                        resolve(bytes);
+                        resolve(null);
                     }
-                } catch (e) { reject(e); }
+                }
             });
         });
     }
@@ -159,15 +160,14 @@ class DashboardIndicator extends PanelMenu.Button {
     _fetchJson(url, token, callback) {
         let msg = Soup.Message.new('GET', url);
         msg.request_headers.append('Authorization', `Bearer ${token}`);
-        _httpSession.send_and_read_async(msg, GLib.PRIORITY_DEFAULT, null, (session, result) => {
-            try {
-                let bytes = session.send_and_read_finish(result);
-                if (msg.status_code === 200) {
-                    // Décodage strict
-                    let text = new TextDecoder().decode(bytes.get_data());
-                    callback(JSON.parse(text));
-                }
-            } catch (e) { console.error(e); }
+        
+        _httpSession.queue_message(msg, (session, message) => {
+             if (message.status_code === 200) {
+                 try {
+                    let data = message.response_body.data;
+                    callback(JSON.parse(data));
+                 } catch(e) { log(e); }
+             }
         });
     }
     
@@ -180,16 +180,23 @@ class DashboardIndicator extends PanelMenu.Button {
     }
 });
 
-export default class DashboardExtension extends Extension {
+// --- STANDARD GNOME 42 (LEGACY) ---
+let _indicator;
+
+function init() {
+    return new Extension();
+}
+
+class Extension {
     enable() {
-        this._indicator = new DashboardIndicator();
-        Main.panel.addToStatusArea(this.uuid, this._indicator);
+        _indicator = new DashboardIndicator();
+        Main.panel.addToStatusArea('logtime-indicator', _indicator);
     }
 
     disable() {
-        if (this._indicator) {
-            this._indicator.destroy();
-            this._indicator = null;
+        if (_indicator) {
+            _indicator.destroy();
+            _indicator = null;
         }
     }
 }
